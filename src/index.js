6e56b14dfec8d6f1352fdc3f98d5a4d6e90a65e8
Dileepjatav <58458243+Dileepjatav@users.noreply.github.com>
2022-09-17 16:42:12 +0530

    kefg

--- src/index.js
+++ src/index.js
@@ -66,9 +66,22 @@ app.post('/sub',(req,res)=>{
     if(typeof num1==='string'||typeof num2==='string'){
         status="error"
         message="Invalid data types"                  
+    } 
+    if((num1>1000000||num2>1000000)&& result>1000000){
+        message="Overflow"
+        status="error"
+    }else
+    
+    if(num1<1000000&&num2<1000000){
+        if(result>1000000){
+            message="Overflow"
+            status="error"          
+        }  
     }
     
-        
+    
+    
+    
     res.json(
         {
             "status":status,
