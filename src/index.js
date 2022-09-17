3ce68c8280e8f56b6ddaf8e16b623a2b73c899e0
Dileep jatav <58458243+Dileepjatav@users.noreply.github.com>
2022-09-17 14:35:36 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,36 +17,31 @@ app.get('/',(req,res)=>{
     res.send("Hello world!.")
 })
 
+let message;
+var result;
+let status;
+
 app.post('/add',(req,res)=>{
- 
     const num1=req.body.num1
-    const num2=req.body.num2
-    
-    let message="the sum of given two numbers";
-    var result=num1+num2;
-    let status="success"
+    const num2=req.body.num2   
+    message="the sum of given two numbers";
+    result=num1+num2;
+    status="success"
 
     if(typeof num1==='string'||typeof num2==='string'){
         status="error"
-        message="Invalid data types"
-                  
-    }
-    if(num1>100000||num2>100000){
+        message="Invalid data types"                  
+    }else 
+    if((num1>100000||num2>100000)&& result>100000){
         message="Overflow"
         status="error"
-        
-
-    }
+    }else
     if(num1<100000||num2<100000){
         if(result>100000){
             message="Underflow"
-            status="error"
-            
-        }
-    
-    }
-    
-
+            status="error"          
+        }  
+    }      
     res.json(
         {
             "status":status,
