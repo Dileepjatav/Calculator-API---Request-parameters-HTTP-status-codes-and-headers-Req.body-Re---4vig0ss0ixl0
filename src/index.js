173907061533a71a4650b00d9b12c4ce58659556
Dileepjatav <58458243+Dileepjatav@users.noreply.github.com>
2022-09-17 18:02:41 +0530

    skug

--- src/index.js
+++ src/index.js
@@ -63,11 +63,10 @@ app.post('/sub',(req,res)=>{
     result=num1-num2;
     status="success"
 
-    if(num1>1000000&&num2>1000000){
-        if(result>1000000){
+    if((num1>1000000&&num2>1000000)&&result>1000000){
+        
             message="Overflow"
             status="error"
-        }
         
     }else
     
@@ -77,12 +76,6 @@ app.post('/sub',(req,res)=>{
             status="error"          
         }  
     } 
-    if(num1<1000000&&num2<1000000){
-        if(result>1000000){
-            message="Overflow"
-            status="error"          
-        }  
-    }
     
     if(typeof num1==='string'||typeof num2==='string'){
         status="error"
