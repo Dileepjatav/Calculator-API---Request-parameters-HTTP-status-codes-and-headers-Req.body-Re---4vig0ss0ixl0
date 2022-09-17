aa92a1a9b5f0ee5133a806142ff23a09aafb6db9
Dileepjatav <58458243+Dileepjatav@users.noreply.github.com>
2022-09-17 16:31:21 +0530

    sjdfg

--- src/index.js
+++ src/index.js
@@ -91,9 +91,13 @@ app.post('/multiply',(req,res)=>{
         status="error"
         message="Invalid data types"                  
     }else 
+    if((num1>1000000||num2>1000000)&& result>1000000){
+        message="Overflow"
+        status="error"
+    }else
     
     if(num1<1000000&&num2<1000000){
-        if(result<1000000){
+        if(result>1000000){
             message="Underflow"
             status="error"          
         }  
@@ -120,14 +124,9 @@ app.post('/divide',(req,res)=>{
     if(typeof num1==='string'||typeof num2==='string'){
         status="error"
         message="Invalid data types"                  
-    }else 
-    if((num1>100000||num2>100000)&& result>100000){
-        message="Overflow"
-        status="error"
-    }else
-    
+    }
     if(num1<100000||num2<100000){
-        if(result<100000){
+        if(result>100000){
             message="Underflow"
             status="error"          
         }  
