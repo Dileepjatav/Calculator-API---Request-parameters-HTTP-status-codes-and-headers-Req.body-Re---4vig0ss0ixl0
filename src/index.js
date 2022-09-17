b98352341df7eeffa72e64853d9aea505a52e125
Dileep jatav <58458243+Dileepjatav@users.noreply.github.com>
2022-09-17 14:13:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,6 +13,51 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 // your code goes here
 
+app.get('/',(req,res)=>{
+    res.send("Hello world!.")
+})
+
+app.post('/add',(req,res)=>{
+ 
+    const num1=req.body.num1
+    const num2=req.body.num2
+    
+    let message="the sum of given two numbers";
+    var result=num1+num2;
+    let status="success"
+
+    if(typeof num1==='string'||typeof num2==='string'){
+        status="error"
+        message="Invalid data types"
+                  
+    }
+    if(num1>100000||num2>100000){
+        message="Overflow"
+        status="error"
+        
+
+    }
+    if(num1<100000||num2<100000){
+        if(result>100000){
+            message="Underflow"
+            status="error"
+            
+        }
+    
+    }
+    
+
+    res.json(
+        {
+            "status":status,
+            "message":message,
+            "sum":result
+        }
+    )
+    res.end();
+
+})
+
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
