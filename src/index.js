9e5bac6c3963991c3a7b63f3a2cb00fc2c4ab376
Dileepjatav <58458243+Dileepjatav@users.noreply.github.com>
2022-09-17 16:10:27 +0530

    sfg

--- src/index.js
+++ src/index.js
@@ -62,18 +62,7 @@ app.post('/sub',(req,res)=>{
     message="the difference of given two numbers";
     result=num1-num2;
     status="success"
-
-    if(num1<1000000||num2<1000000){
-        if(result<1000000){
-            message="Underflow"
-            status="error"          
-        }  
-    }  
-
-    if((num1>1000000||num2>1000000)&& result<1000000){
-        message="Overflow"
-        status="error"
-    }
+    
     if(typeof num1==='string'||typeof num2==='string'){
         status="error"
         message="Invalid data types"                  
